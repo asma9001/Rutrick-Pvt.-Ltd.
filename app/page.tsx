a62269f845fe5b1c8506817a212bb08a624a06ee
Asma Siddique <asmasiddique44097@gmail.com>
2025-08-26 14:14:20 +0500

    first commits

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 // import HeroSection from "@/components/hero-section"
 import AboutSection from "@/components/about-section"
 import ServicesSection from "@/components/services-section"
-import TestimonialsSection from "@/components/testimonials-section"
+
 import ContactSection from "@/components/contact-section"
 import Footer from "@/components/footer"
 import Navigation from "@/components/navigations"
@@ -15,7 +15,7 @@ export default function RutrickWebsite() {
       <HeroSection />
       <AboutSection />
       <ServicesSection />
-      {/* <TestimonialsSection /> */}
+      
       <ContactSection />
       <Footer />
     </div>
